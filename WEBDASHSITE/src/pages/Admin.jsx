039d8d91d759d43a2fb4ad28279a263b40ca96fd
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-04-18 16:14:24 +0530

    changes in the backend

--- WEBDASHSITE/src/pages/Admin.jsx
+++ WEBDASHSITE/src/pages/Admin.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import Navbar from '../Components/navbar';
 import SongManagement from "../Components/songmanagement";
 import DiaryManagement from "../Components/dairymanagement";
+import { useState, useEffect } from "react";
 
-const Admin = () => {
+
+
+const Admin = () => { 
+
+    const [role, setRole] = useState(null);
+    const [email, setEmail] = useState(null);
+
+    useEffect(() => {
+        const userRole = localStorage.getItem('role');
+        const userEmail = localStorage.getItem('email');
+        setRole(userRole);
+        setEmail(userEmail);
+    },[])
+
+
     return (
     <>
         <Navbar/>
-        <SongManagement/>
+        {role === "admin" && <SongManagement/>}
         <DiaryManagement/>
     </>
     )
